db3705c5b78947d05b6375e6e144deb56775b81f
Gustavo Leitão <gustavo.leitao@imd.ufrn.br>
2025-10-15 18:02:25 -0300

    aula 08

--- aula08/inteiro.c
+++ aula08/inteiro.c
@@ -2,9 +2,9 @@
 
 int main(){
 
-    short int pequeno = 10; // 2 bytes | 32768
-    int medio = 10; // 4 bytes | 2147483648
-    long int grande = 0; //8 bytes | 9223372036854775808
+    unsigned short int pequeno = 10; // 2 bytes | 32768 | 65536
+    unsigned int medio = 10; // 4 bytes | 2147483648 | 
+    unsigned long int grande = 0; //8 bytes | 9223372036854775808
 
     scanf("%hd", &pequeno);
     printf("%hd\n", pequeno);
